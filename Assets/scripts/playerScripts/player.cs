e65deb5514f7a3ea495c4716c4bcff0e6db7c22e
Benjamin Santos <aluckyblock9098@gmail.com>
2022-10-19 17:40:59 -0500

    yes

---
+++ Assets/scripts/playerScripts/player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    int maxZDistace = 0;
+    public scoreManager scrManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxZDistace = (int)this.gameObject.transform.position.z;
+        scrManager.addScore(10);
+        scrManager.addScore(20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreUpdater();
+    }
+    void scoreUpdater() {
+        if (maxZDistace < (int)this.gameObject.transform.position.z)
+        {
+            maxZDistace = (int)this.gameObject.transform.position.z;
+            scrManager.addScore(1);
+        }
+        
+    }
+}
