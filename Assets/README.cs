b3af7e0975d4f651a94c718218e7754da8fba2b3
Benjamin Santos <67348724+programmableGun@users.noreply.github.com>
2022-10-21 18:27:03 -0500

    hehhe

--- Assets/README.cs
+++ Assets/README.cs
@@ -1,26 +1,13 @@
 /*
-<<<<<<< HEAD
 Add in a change level function, as the levels go up the more blocks spawn and the level is longer
 make a new level generator, make segmements for the level generator to copy
 make the bean explode when it dies
 add combo counter text
-=======
  Gameplay:
 make the bean explode when it dies
 Change Level every time the player hits the end of the level. 
 add combo counter
->>>>>>> 977e558d0e34161c124593e4a800116a067951d8
 
-<<<<<<< HEAD
-       
-=======
-    Level Generation:
-Change the level Generator, instead of complete randomness, the generator will generate chuncks that have been preloaed into the script
->>>>>>> 977e558d0e34161c124593e4a800116a067951d8
+     hehehhehe  
 
-<<<<<<< HEAD
-=======
-    hehehehheheheh
-
->>>>>>> 977e558d0e34161c124593e4a800116a067951d8
 */
