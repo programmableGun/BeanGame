7d8de52dbb06f974a30fb23d90d97c0e264c71a9
Benjamin Santos <aluckyblock9098@gmail.com>
2022-10-21 17:57:51 -0500

    list of things todo

--- Assets/README.cs
+++ Assets/README.cs
@@ -1,7 +1,12 @@
 ﻿/*
+ Gameplay:
+make the bean explode when it dies
+Change Level every time the player hits the end of the level. 
+add combo counter
 
-Change Level
+    Level Generation:
+Change the level Generator, instead of complete randomness, the generator will generate chuncks that have been preloaed into the script
 
-
+    
 
 */
