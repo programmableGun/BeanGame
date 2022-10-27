e8f8bb86f1f1a23d16328cd51c89fadef9298f83
Benjamin Santos <67348724+programmableGun@users.noreply.github.com>
2022-10-27 13:22:53 -0500

    started working on menu script

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject menuScreen;
+
+    // what happends when the pause button is pressed
+
+    // what happends when the play button is pressed
+    public void PlayPressed(){
+        menuScreen.SetActive(false);
+    }
+
+    //what happends when the settings button is pressed
+    
+    
+    //what happends when the skins button is pressed
+    
+    
+    // what happends when the exit button is pressed
+
+
+
+
+
+
+
+    void Start(){
+
+    }
+
+    void Update(){
+
+    }
+}
